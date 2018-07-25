d78da6e992a924b0f53ad968120d27c7ffca2c80
asayamakk <akkdkb@gmail.com>
2018-07-25 17:57:55 +0900

    update

--- alias.sh
+++ alias.sh
@@ -1,12 +1,10 @@
-source ~/dotfiles/funcions.sh
+source ~/dotfiles/functions.sh
 
 alias etu='perl -MEncode -nle '\''binmode STDOUT, ":utf8"; print Encode::decode('\''eucjp'\'', $_)'\'''
 alias kabe='open /System/Library/CoreServices/ScreenSaverEngine.app'
 alias grep='ggrep'
 alias apps='sh ~/scripts/startup.sh'
-alias g='cd $(ghq root)/$(ghq list | peco)'
-alias b='hub browse $(ghq list | peco | cut -d "/" -f 2,3)'
-alias v='vim $(ghq root)/$(ghq list | peco)'
+alias g='func_cd_ghq_list'
 alias kanban="open  https://sd-redmine.gmo-media.jp/rb/projects/pointtown/taskboard"
 alias ll="ls -la"
 alias netstat-lntp='sudo lsof -nP -iTCP -sTCP:LISTEN'
