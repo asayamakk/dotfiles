8d79199aa0f97136000fc6c3884fdceee991853c
asayamakk <akkdkb@gmail.com>
2018-11-16 18:33:50 +0900

    peco to fzf

--- functions.sh
+++ functions.sh
@@ -1,5 +1,5 @@
 function func_cd_ghq_list () {
-  selected=$(ghq list | peco)
+  selected=$(ghq list | fzf)
   [ -z $selected ] && change_to=$(pwd) || change_to=$(ghq root)/$selected
   cd $change_to
 }
